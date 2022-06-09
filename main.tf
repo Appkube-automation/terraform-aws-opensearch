b2b9e2b84592aa054af9c58a4d05a869b94ba068
mohdzakir12 <zakir.mohd@synectiks.com>
2022-06-09 11:01:18 +0530

    making it dynamic

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 }
 
 resource "aws_opensearch_domain" "example" {
-  domain_name    = "example"
-  engine_version = "Elasticsearch_7.10"
+  domain_name    = var.domain_name
+  engine_version = var.engine_version
 
   cluster_config {
     dedicated_master_enabled = false
-    instance_count           = 3
-    instance_type            = "r4.large.search"
+    instance_count           = var.instance_count
+    instance_type            = var.instance_type
     cold_storage_options {
       enabled = false
     }
@@ -19,24 +19,24 @@ resource "aws_opensearch_domain" "example" {
   ebs_options {
     ebs_enabled = false
     #  iops        =  
-    #  volume_size = 
-    #  volume_type = 
+    volume_size = var.volume_size
+    volume_type = var.volume_type
   }
   domain_endpoint_options {
-    //custom_endpoint                 = var.custom_endpoint
-    //custom_endpoint_certificate_arn = var.custom_endpoint_certificate_arn
-    custom_endpoint_enabled         = var.custom_endpoint_enabled
-    //enforce_https                   = var.enforce_https
+    custom_endpoint = var.custom_endpoint_enabled ? var.custom_endpoint : null
+    //custom_endpoint_certificate_arn = var.custom_endpoint_enabled?var.custom_endpoint_certificate_arn:null
+    custom_endpoint_enabled = var.custom_endpoint_enabled
+    enforce_https                   = var.enforce_https
     //tls_security_policy             = var.tls_security_policy
   }
 
   encrypt_at_rest {
-           enabled    = var.encrypt_at_rest
-          #  kms_key_id = var.NEED_TO_ADDRESS
-        }
+    enabled = var.encrypt_at_rest
+    #  kms_key_id = var.NEED_TO_ADDRESS
+  }
   node_to_node_encryption {
-           enabled = var.node_encryption
-        }
+    enabled = var.node_encryption
+  }
   tags = {
     Domain = "TestDomain"
   }
